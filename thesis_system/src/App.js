d79e394db9883739422b7f4de0f69a36603946d7
kushcrisal <074bct519.kushal@pcampus.edu.np>
2021-06-18 09:52:52 +0545

    tracked app.js

---
+++ thesis_system/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+import Route from "./components/Navbar/Routee"
+
+
+export default function App() {
+    return (
+        <Route />
+    )
+}
